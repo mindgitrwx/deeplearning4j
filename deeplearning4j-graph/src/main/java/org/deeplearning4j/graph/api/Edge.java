7d23b4df71be7b309e9a1d7c8fe6be5d565194e1
agigom2233 <agigom1993@gmail.com>
2018-05-07 20:53:01 +0900

    1.Self Encapsulate Field 2.Class Edge 3.Add get function to each private member to encapsulation.

--- deeplearning4j-graph/src/main/java/org/deeplearning4j/graph/api/Edge.java
+++ deeplearning4j-graph/src/main/java/org/deeplearning4j/graph/api/Edge.java
@@ -1,7 +1,6 @@
 package org.deeplearning4j.graph.api;
 
 import lombok.Data;
-
 /** Edge in a graph. May be a directed or undirected edge.<br>
  * Parameterized, and may store a value/object associated with the edge
  */
@@ -20,10 +19,26 @@ public class Edge<T> {
         this.directed = directed;
     }
 
+    public int getFrom(){
+        return from;
+    }
+
+    public int getTo(){
+        return to;
+    }
+
+    public T getValue(){
+        return value;
+    }
+
+    public boolean getDirected(){
+        return directed;
+    }
+
     @Override
     public String toString() {
         return "edge(" + (directed ? "directed" : "undirected") + "," + from + (directed ? "->" : "--") + to + ","
-                        + (value != null ? value : "") + ")";
+                + (value != null ? value : "") + ")";
     }
 
     @Override
@@ -31,36 +46,36 @@ public class Edge<T> {
         if (!(o instanceof Edge))
             return false;
         Edge<?> e = (Edge<?>) o;
-        if (directed != e.directed)
+        if (getDirected() != e.getDirected())
             return false;
-        if (directed) {
-            if (from != e.from)
+        if (getDirected()) {
+            if (getFrom() != e.getFrom())
                 return false;
-            if (to != e.to)
+            if (getTo() != e.getTo())
                 return false;
         } else {
-            if (from == e.from) {
-                if (to != e.to)
+            if (getFrom() == e.getFrom()) {
+                if (getTo() != e.getTo())
                     return false;
             } else {
-                if (from != e.to)
+                if (getFrom() != e.getTo())
                     return false;
-                if (to != e.from)
+                if (getTo() != e.getFrom())
                     return false;
             }
         }
-        if ((value != null && e.value == null) || (value == null && e.value != null))
+        if ((getValue() != null && e.getValue() == null) || (getValue() == null && e.getValue() != null))
             return false;
-        return value == null || value.equals(e.value);
+        return getValue() == null || getValue().equals(e.getValue());
     }
 
     @Override
     public int hashCode() {
         int result = 17;
-        result = 31 * result + (directed ? 1 : 0);
-        result = 31 * result + from;
-        result = 31 * result + to;
-        result = 31 * result + (value == null ? 0 : value.hashCode());
+        result = 31 * result + (getDirected() ? 1 : 0);
+        result = 31 * result + getFrom();
+        result = 31 * result + getTo();
+        result = 31 * result + (getValue() == null ? 0 : getValue().hashCode());
         return result;
     }
 }
